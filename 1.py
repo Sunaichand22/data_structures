6f452a916360b930274a3deaed299d090cb04a66
Sunaichand22 <49715853+Sunaichand22@users.noreply.github.com>
2019-09-09 15:50:16 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> num = 8 
+#num = float(input('Enter a number: '))
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
---
+++ gcd.py
@@ -0,0 +1,4 @@
+import fractions
+a=int(input("Enter the Value for a:"))
+b=int(input("Enter the Value for b:"))
+print("The GCD of two numbers is: ",fractions.gcd(a,b))
